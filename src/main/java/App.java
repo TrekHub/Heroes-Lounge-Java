96de15a2c6431cb5ad488ac6f38a2c80630f1e2f
TrekHub <teddyomondi001@gmail.com>
2022-02-02 21:13:48 +0300

    creating the success page route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,18 +1,25 @@
 import models.Team;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
-
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
 import static spark.Spark.*;
 
 
+
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
 
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
-
         //Root Route
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
@@ -23,35 +30,40 @@ public class App {
         //Hero Route
         get("/heroes", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
-
             return new ModelAndView(model, "heroes.hbs");
         }, new HandlebarsTemplateEngine());
 
 
-        //Teams Route
+        //Form route
+        get("/form", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "form.hbs");
+        }, new HandlebarsTemplateEngine());
+
         get("/teams", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
-
+            ArrayList<Team> allTeams = Team.getAllTeams();
+            model.put("allTeams", allTeams);
             return new ModelAndView(model, "teams.hbs");
         }, new HandlebarsTemplateEngine());
 
 
-        post("/new/", (request, response) -> {
+
+        post("/getTeams/new", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
-            String name = request.params("name");
-            String maxMembers = request.params("maxMembers");
-            String cause = request.params("cause");
-
-
-
-
-            Team team = new Team(name, maxMembers, cause);
-
-
-            return  new ModelAndView(model, "index.hbs");
-
-
+            String name = request.queryParams("name");
+            String cause = request.queryParams("cause");
+            String maxMembers = request.queryParams("maxMembers");
+            Team resume = new Team(name, cause, maxMembers);
+            return new ModelAndView(model, "heroes.hbs");
         }, new HandlebarsTemplateEngine());
+
+
+
+
+
+
+
 
     }
 }
